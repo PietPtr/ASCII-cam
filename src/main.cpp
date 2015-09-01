ba90e960db483dbdf85387b7c644720557af3bae
Pieter <pietdagamer@gmail.com>
2015-09-01 09:18:16 +0200

    Use C++ interface instead of C

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,33 @@
 #include <opencv2/imgproc/imgproc.hpp>  // Gaussian Blur
 #include <opencv2/highgui/highgui.hpp>  // OpenCV window I/O
 
+//Laptop webcam size: 320x240
+
+using namespace cv;
+
+int main()
+{
+    VideoCapture cap(-1);     // get 'any' cam
+    while( cap.isOpened() )   // check if we succeeded
+    {
+        Mat frame;
+        if ( ! cap.read(frame) )
+            break;
+        imshow("lalala",frame);
+        int k = waitKey(33);
+        if ( k==27 )
+            break;
+    }
+    return 0;
+}
+
+
+
+
+
+
+/* Outdated C interface \/
+
 using namespace cv;
 
 int main(int argc,char *argv[])
@@ -12,18 +39,21 @@ int main(int argc,char *argv[])
     IplImage* color_img;
     CvCapture* cv_cap = cvCaptureFromCAM(0);
     cvNamedWindow("Float",0); // create window
-    for(;;) {
+    while (true)
+    {
         color_img = cvQueryFrame(cv_cap); // get frame
         if(color_img != 0)
             cvShowImage("Float", color_img); // show frame
-
+        //std::cout << color_img->at<char>(20, 20) << "\n";
         c = cvWaitKey(10); // wait 10 ms or for key stroke
         if(c == 27)
             break; // if ESC, break and quit
     }
-    /* clean up */
+
+    // clean up
     cvReleaseCapture( &cv_cap );
     cvDestroyWindow("Float");
 
     return 0;
-}
+}*/
+
